c8dc57ef93645d6008d7a2761390a559d8a5dbf0
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-11 08:46:07 -0700

    docstring improved

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-"""The *main* module that allows us to kick off the application."""
+"""The *main* module that allows us to kick off the application.
+
+**NOTE:**
+- Because this application fakes its streamed data,
+  the symols in *STREAM_SYMBOLS* will make no difference to
+  what is streamed.
+- They **WILL** make a difference to what is processed.
+- Currently, you will get messages for APPL & AMZN, but nothing for DUD
+  as there is no fake streamed data created for it. You can make some!
+  - This demonstrates a situation, where you are streaming a
+    symbol with no current price action.
+"""
 
 from actors.administrator import Administrator
 
 if __name__ == '__main__':
 
-    STREAM_SYMBOLS = ('AAPL', 'TSLA', 'AMZN')
+    STREAM_SYMBOLS = ('AAPL', 'DUD', 'AMZN')
 
     print('Running... Press Ctrl-C to stop.')
 
