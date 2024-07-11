a350a781516008c1f8375ab8033d41724e720845
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-10 20:15:04 -0700

    ResultInstance fields added

--- actors/helpers/named_tuples.py
+++ actors/helpers/named_tuples.py
@@ -1,13 +1,26 @@
 """Module containing NamedTuple classes for the *actors* module."""
 
-from typing import NamedTuple
+from __future__ import annotations
+from typing import TYPE_CHECKING, NamedTuple
+
+if TYPE_CHECKING:
+    from datetime import datetime
+
 
 class ErrorInstance(NamedTuple):
     """NamedTuple class for returning *error* details."""
 
     error: Exception
 
+
 class ResultInstance(NamedTuple):
     """NamedTuple class for returning *result* details."""
 
-    pass # TODO
+    symbol: str
+    open: float
+    high: float
+    low: float
+    close: float
+    volume: float
+    unix_ts: int
+    new_york_datetime: datetime
