91fcd7288454341dd277907cd0a41cc4c1f41000
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-10 19:12:30 -0700

    result queue passed into SymbolWorker

--- actors/symbol_worker.py
+++ actors/symbol_worker.py
@@ -1,5 +1,11 @@
 """Module containing the *SymbolWorker* class."""
 
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ray.util.queue import Queue
+
 class SymbolWorker:
     """Class responsible for *processing* a specific streamed data symbol.
     
@@ -7,7 +13,7 @@ class SymbolWorker:
     is a toy example, we are going allow it to be discarded, once it is processed.
     """
 
-    def __init__(self, symbol: str) -> None:
+    def __init__(self, symbol: str, result_queue: Queue) -> None:
         """Initialize the class.
         
         Args:
@@ -15,6 +21,7 @@ class SymbolWorker:
         """
 
         self._symbol = symbol
+        self._result_queue = result_queue
     
     def process_latest_period(self, latest_period: dict) -> None:
         """Process the latest streamed period data.
@@ -27,6 +34,10 @@ class SymbolWorker:
 
         if not symbol_latest_period:
             return
+        
+        # TODO: Process the latest period data
+        
+        self._result_queue.put(symbol_latest_period)
 
         print(self._symbol)
         print(symbol_latest_period)
