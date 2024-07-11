4404f9087982d184d914045a6fca377105a06bca
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-11 06:31:14 -0700

    stream_symbols added as init param

--- actors/streamer.py
+++ actors/streamer.py
@@ -12,7 +12,17 @@ from data.fake_api_data import FAKE_DATA
 class Streamer:
     """Class responsible for streaming data."""
 
-    def __init__(self) -> None:
+    def __init__(self, stream_symbols: tuple) -> None:
+        """Initialize the class.
+
+        Args:
+            stream_symbols: The symbols to stream.
+        
+        **NOTE:**
+        - Because this is a fake API, the symbols' data it returns
+          is predefined.
+        - Typically, you would subscribe to the symbols in *stream_symbols*.
+        """
 
         self._period_number = 0
 
@@ -29,5 +39,5 @@ class Streamer:
             raise ConnectionError('No more data available from API.')
         else:
             self._period_number += 1
-            return latest_ohlcv_data # TODO: Serialize
+            return latest_ohlcv_data
 
